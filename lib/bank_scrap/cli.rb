0e721a2891e41d07c64c94d7b070c62daf97ccc7
Juanma <juanma@aspgems.com>
2015-03-13 18:05:28 +0100

    Fixed CLI input params.

--- lib/bank_scrap/cli.rb
+++ lib/bank_scrap/cli.rb
@@ -13,6 +13,9 @@ module BankScrap
       # Usage:
       # bank_scrap balance BANK_NAME --extra=birthday:01/12/1980
       option :extra, type: :hash, default: {}
+      option :iban, type: :string, default: nil
+      option :from, type: :string, default: nil
+      option :to, type: :string, default: nil
     end
 
     desc "balance BANK", "get accounts' balance"
@@ -29,39 +32,21 @@ module BankScrap
 
     desc "transactions BANK", "get account's transactions"
     shared_options
-    def transactions(bank, iban = nil)
+    def transactions(bank)
       assign_shared_options
-    
-      begin 
-        start_date = @extra_args.has_key?('from') ? Date.strptime(@extra_args['from'],'%d-%m-%Y') : nil
-        end_date = @extra_args.has_key?('to') ? Date.strptime(@extra_args['to'],'%d-%m-%Y') : nil
-      rescue ArgumentError
-        say "Invalid date format. Correct format d-m-Y", :red
-      end
-
       initialize_client_for(bank)
 
-      account = iban ? @client.account_with_iban(iban) : @client.accounts.first
-
-      if (!start_date.nil? && !end_date.nil?)
-        if (start_date > end_date)
-          say "From date must be lower than to date", :red
-          exit
-        end
-
-        transactions = account.fetch_transactions(start_date:start_date, end_date:end_date)
-      else
-        transactions = account.transactions
-      end
+      account = @iban ? @client.account_with_iban(@iban) : @client.accounts.first
+      transactions = account.fetch_transactions(start_date: @from, end_date: @to)
 
       say "Transactions for: #{account.description} (#{account.iban})", :cyan
-      
+
       transactions.each do |transaction|
         say transaction.to_s, (transaction.amount > Money.new(0) ? :green : :red)
       end
     end
 
-    private 
+    private
 
     def assign_shared_options
       @user       = options[:user]
@@ -69,13 +54,25 @@ module BankScrap
       @log        = options[:log]
       @debug      = options[:debug]
       @extra_args = options[:extra]
+      @iban       = options[:iban]
+      begin
+        @from = options[:from] ? Date.strptime(options[:from], '%d-%m-%Y') : Date.today - 1.month
+        @to = options[:to] ? Date.strptime(options[:to], '%d-%m-%Y') : Date.today
+        if @from > @to
+          say "From date must be lower than to date", :red
+          exit
+        end
+      rescue ArgumentError
+        say "Invalid date format. Correct format d-m-Y", :red
+        exit
+      end
     end
 
     def initialize_client_for(bank_name)
-      bank_class = find_bank_class_for(bank_name)      
+      bank_class = find_bank_class_for(bank_name)
       @client = bank_class.new(@user, @password, log: @log, debug: @debug, extra_args: @extra_args)
     end
-    
+
     def find_bank_class_for(bank_name)
       Object.const_get("BankScrap::" + bank_name.classify)
     rescue NameError
